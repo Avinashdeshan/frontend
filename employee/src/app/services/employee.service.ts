c0e74fc450cd26a8a7f058856a9bbfb5b51c2b49
Avinash <avinashdeshan11@gmail.com>
2025-04-29 16:11:23 +0530

    create employee.service.ts

--- employee/src/app/services/employee.service.ts
+++ employee/src/app/services/employee.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../model/employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private apiUrl='http://localhost:8080/api/employees';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAll(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.apiUrl);
+  }
+
+  create(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(this.apiUrl, employee);
+  }
+
+  update(id: number, employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiUrl}/${id}`, employee);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
