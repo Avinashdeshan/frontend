83ac4896d49b8ce609245f03e8c45209176b3355
Avinash <avinashdeshan11@gmail.com>
2025-04-29 16:43:06 +0530

    create emp-form.components.ts

--- employee/src/app/components/employee-form/employee-form.component.ts
+++ employee/src/app/components/employee-form/employee-form.component.ts
@@ -1,4 +1,8 @@
+import { routes } from './../../app.routes';
 import { Component } from '@angular/core';
+import { Employee } from '../../model/employee.model';
+import { EmployeeService } from '../../services/employee.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-form',
@@ -7,5 +11,18 @@ import { Component } from '@angular/core';
   styleUrl: './employee-form.component.css'
 })
 export class EmployeeFormComponent {
+  employee:Employee={
+    name: '',
+    email: '',
+    department: ''
+  };
+  departments = ['HR', 'IT', 'Finance', 'Operations'];
 
+  constructor(private employeeService:EmployeeService, private router:Router){}
+
+  onSubmit(): void {
+    this.employeeService.create(this.employee).subscribe(() => {
+      this.router.navigate(['/']);
+    });
+  }
 }
