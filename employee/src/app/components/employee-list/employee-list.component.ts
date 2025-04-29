3ff6fbf34797b2bb4c22263512cbc63bba24b865
Avinash <avinashdeshan11@gmail.com>
2025-04-29 16:23:01 +0530

    implement emp-list.components.ts

--- employee/src/app/components/employee-list/employee-list.component.ts
+++ employee/src/app/components/employee-list/employee-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../model/employee.model';
+import { EmployeeService } from '../../services/employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,6 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit{
+  employees: Employee[]=[];
 
+  constructor(private employeeService: EmployeeService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadEmployees();
+  }
+
+  loadEmployees():void {
+    this.employeeService.getAll().subscribe(data=> this.employees=data);
+  }
+
+  deleteEmployee(id:number):void{
+    if (confirm('Are you sure')){
+      this.employeeService.delete(id).subscribe(()=>this.loadEmployees());
+    }
+  }
+
+  goToAddFor(){
+    this.router.navigate(['/add'])
+  }
 }
